bb3c03de30ac7478ba88481af4dd497b60fcad83
eliezerdeleon <djnb.nickyblast@gmail.com>
2022-12-03 05:05:22 -0400

    chromedriver updated to v108

--- app/src/test/java/pages/BasePage.java
+++ app/src/test/java/pages/BasePage.java
@@ -14,7 +14,7 @@ public class BasePage {
     private static WebDriverWait wait;
 
     static {
-        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver\\chromedriver_108_0535971.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
         driver = new ChromeDriver(chromeOptions);
         wait = new WebDriverWait(driver, 10);
